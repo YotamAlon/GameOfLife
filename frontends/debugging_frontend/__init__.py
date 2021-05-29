789b02a6348941829aefbab5427daacc784588ec
Yotam Alon <yotamalon@gmail.com>
2021-05-29 17:56:20 +0300

    Fixed game logic, improved debugging frontend output format and added logging

--- frontends/debugging_frontend/__init__.py
+++ frontends/debugging_frontend/__init__.py
@@ -2,29 +2,17 @@ from frontend_base import BaseFrontend, Instruction
 from game_logic import Grid, Cell
 
 
-class TextColor(object):
-    base_sequence = '\033[{}m'
-    default = white = '0'
-    black = '47'
-
-    def paint_text(self, text, color):
-        color_code = getattr(self, color, None)
-        if color_code is None:
-            raise ValueError(f'TextColor does not support {color}')
-        return f'{self.base_sequence.format(color_code)}{text}{self.base_sequence.format(self.default)}'
-
-
 class DebuggingFrontend(BaseFrontend):
     def draw_grid(self, grid: Grid):
-        [print(TextColor().paint_text(str(cell), 'black' if cell.is_alive else 'white'))
-         for cell in grid.get_living_cells()]
+        for row in grid._grid:
+            print('  '.join(['■' if cell.is_alive else '□' for cell in row]))
 
     def get_initial_game_state(self) -> Grid:
         grid = Grid()
         more_input_remains = True
         while more_input_remains:
             self.draw_grid(grid)
-            next_input = input('Enter the coordinates for next cell to turn alive (in the format "x,y")'
+            next_input = input('Enter the coordinates for next cell to turn alive (in the format "x,y") '
                                'or nothing to start the game:\n >>> ')
             if len(next_input) > 0:
                 try:
@@ -39,8 +27,7 @@ class DebuggingFrontend(BaseFrontend):
         return grid
 
     def get_next_instruction(self) -> Instruction:
-        try:
-            input('Press enter to progress one generation or ctrl + c to exit')
-        except KeyboardInterrupt:
+        next_input = input('Press enter to progress one generation or e to exit')
+        if next_input == 'e':
             return Instruction(type_='exit')
         return Instruction(type_='next')
--- game_logic.py
+++ game_logic.py
@@ -1,3 +1,6 @@
+from logging import getLogger
+
+
 class Cell(object):
     def __init__(self, x: int, y: int, is_alive=False):
         self.x = x
@@ -5,14 +8,17 @@ class Cell(object):
         self.is_alive = is_alive
 
     def __str__(self):
-        return f'{self.x}, {self.y}, {"alive" if self.is_alive else "dead"}'
+        return f'Cell(x={self.x}, y={self.y}, {"alive" if self.is_alive else "dead"})'
+
+    def __repr__(self):
+        return str(self)
 
     def set_is_alive(self, is_alive):
         self.is_alive = is_alive
 
 
 class Grid(object):
-    def __init__(self, size: int=10) -> None:
+    def __init__(self, size: int = 10) -> None:
         self.size = size
         self._grid = [[Cell(x, y) for x in range(self.size)] for y in range(self.size)]
 
@@ -20,24 +26,26 @@ class Grid(object):
         self._grid[cell.x][cell.y].set_is_alive(is_alive)
 
     def get_living_cells(self):
-        for column in self._grid:
-            for cell in column:
+        for row in self._grid:
+            for cell in row:
                 if cell.is_alive:
                     yield cell
 
     def get_living_neighbors(self, cell):
         for dx in range(-1, 2):
             for dy in range(-1, 2):
-                if dx != 0 and dy != 0:
-                    try:
-                        if self._grid[cell.x + dx][cell.y + dy].is_alive:
-                            yield self._grid[cell.x + dx][cell.y + dy]
-                    except IndexError:
-                        pass
+                if dx == 0 and dy == 0:
+                    continue
+
+                try:
+                    if self._grid[cell.x + dx][cell.y + dy].is_alive:
+                        yield self._grid[cell.x + dx][cell.y + dy]
+                except IndexError:
+                    pass
 
     def get_dead_cells(self):
-        for column in self._grid:
-            for cell in column:
+        for row in self._grid:
+            for cell in row:
                 if not cell.is_alive:
                     yield cell
 
@@ -49,13 +57,21 @@ class GameOfLifeEngine(object):
     def calculate_next_game_state(self) -> Grid:
         new_grid = Grid()
         for cell in self.grid.get_living_cells():
-            n_neighbors = len(list(self.grid.get_living_neighbors(cell)))
+
+            neighbors = list(self.grid.get_living_neighbors(cell))
+            getLogger().debug(f'Cell {cell} has neighbors: {neighbors}')
+            n_neighbors = len(neighbors)
             if 2 <= n_neighbors <= 3:
+                getLogger().info(f'setting {cell} as alive because it has {n_neighbors} neighbors')
                 new_grid.set_cell_life(cell, is_alive=True)
 
         for cell in self.grid.get_dead_cells():
-            n_neighbors = len(list(self.grid.get_living_neighbors(cell)))
+            getLogger().debug(f'looking at {cell}')
+            neighbors = list(self.grid.get_living_neighbors(cell))
+            getLogger().debug(f'Cell {cell} has neighbors: {neighbors}')
+            n_neighbors = len(neighbors)
             if n_neighbors == 3:
+                getLogger().info(f'setting {cell} as alive because it has {n_neighbors} neighbors')
                 new_grid.set_cell_life(cell, is_alive=True)
 
         return new_grid
