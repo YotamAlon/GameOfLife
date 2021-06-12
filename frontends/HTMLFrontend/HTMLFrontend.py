a662d1d5758ce23399e1d0c3888313ca7eca647f
Yotam Alon <yotamalon@gmail.com>
2021-06-12 09:36:37 +0300

    Refactored HTMLFrontend

--- frontends/HTMLFrontend/HTMLFrontend.py
+++ frontends/HTMLFrontend/HTMLFrontend.py
@@ -9,19 +9,26 @@ class HTMLFrontend(BaseFrontend):
     def initialize(self, initial_state: BaseModel) -> None:
         self.view_state = initial_state
         self.app = Flask(__name__)
-        url_func_map = {'/': self.index, '/set/<string:coordinates>': self.set_, '/next': self.next_,
-                        '/exit': self.exit}
-
-        for url, func in url_func_map.items():
+        for url, func in self.urls.items():
             self.app.route(url)(func)
 
+    @property
+    def html_template(self):
+        return 'index.html'
+
+    @property
+    def urls(self):
+        return {'/': self.index, '/set/<string:coordinates>': self.set_, '/next': self.next_,
+                '/exit': self.exit}
+
     def index(self):
+        data = self.get_index_data()
+        return render_template(self.html_template, **data)
+
+    def get_index_data(self):
         grid = self.generate_grid(self.view_state)
-        # return tabulate([[f'{y}'] + [self.turn_on_button(cell) for cell in grid[i]]
-        #                  for i, y in enumerate(self.view_state.y_range)],
-        #                 headers=[' '] + [f'{x}' for x in self.view_state.x_range], tablefmt='unsafehtml')
-        return render_template('index.html', grid=grid, x_range=self.view_state.x_range,
-                               y_range=self.view_state.y_range)
+        data = {'grid': grid, 'x_range': self.view_state.x_range, 'y_range': self.view_state.y_range}
+        return data
 
     def set_(self, coordinates: str):
         x, y = coordinates.split(',')
